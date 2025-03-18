20b67a44c1df44c0805e9fa92a658992f943a7e0
akamit95856 <121858421+akamit95856@users.noreply.github.com>
2025-03-18 13:50:39 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="ML_Project_Sigmoid_Analytics",
+    version="0.0.0",
+    author="Amit",
+    author_email="akamitnew958@gmail.com",
+    packages=find_packages()
+)
